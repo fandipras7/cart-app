7f463efe4734a973690e1b8d43068e7451d68eb6
fandipras7 <fandiprasetyopro@gmail.com>
2022-12-15 08:46:42 +0700

    make app as homepage

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}></Route>
+      </Routes>
+    </BrowserRouter>
   </Provider>
 );
 
